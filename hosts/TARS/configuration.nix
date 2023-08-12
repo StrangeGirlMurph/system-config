044e9b37a0ddf5c72d156866963071811a301bc1
Murphy <git@murphy-in.space>
2023-08-12 10:23:30 +0200

    game dev

--- hosts/TARS/configuration.nix
+++ hosts/TARS/configuration.nix
@@ -9,6 +9,10 @@
   networking.hostName = "TARS";
 
   environment.systemPackages = with pkgs; [
+    # Game development
+    unityhub # Unity game engine
+    godot_4 # Godot game engine
+
     # Games
     steam # Game client
     desmume # Nintendo DS emulator
