da26fd5b6782ae0aaefb6004a2054332b28ad0dd
Murphy <git@murphy-in.space>
2023-08-13 16:07:49 +0200

    fix path

--- config/home.nix
+++ config/home.nix
@@ -6,9 +6,9 @@
   home.stateVersion = "23.05";
   home.packages = with pkgs; [ ];
 
-  home.file.".config/hypr/wallpaper.jpg".source = "../dotfiles/hypr/wallpaper.jpg";
-  home.file.".config/hypr/hyprpaper.conf".source = "../dotfiles/hypr/hyprpaper.conf";
-  home.file.".config/neofetch/config.conf".source = "../dotfiles/neofetch/config.conf";
+  home.file.".config/hypr/wallpaper.jpg".source = ../dotfiles/hypr/wallpaper.jpg;
+  home.file.".config/hypr/hyprpaper.conf".source = ../dotfiles/hypr/hyprpaper.conf;
+  home.file.".config/neofetch/config.conf".source = ../dotfiles/neofetch/config.conf;
 
   imports = [
     ./desktop-environment.nix
