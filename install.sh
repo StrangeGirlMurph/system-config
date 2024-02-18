06b7da68a4fc5e2e15950baa86ff9603a226ddcc
Murphy <git@murphy-in.space>
2024-02-18 12:20:32 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~
+nix-shell -p git --run "git clone https://github.com/StrangeGirlMurph/system-config"
+sudo mv /etc/nixos ~/system-config/nixos-config.bak
+sudo ln -s ~/system-config/ /etc/nixos
+sudo nixos-rebuild switch
