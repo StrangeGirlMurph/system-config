33d510af5c7e951894be8d29d08235621be93af1
Murphy <git@murphy-in.space>
2024-04-17 14:13:34 +0200

    Generation #9

--- update-system.fish
+++ update-system.fish
@@ -1,14 +1,16 @@
 pushd ~/system-config
 nixpkgs-fmt . &>/dev/null
-git diff -U0
+git diff -U1
 echo "Rebuilding NixOS..."
+git add .
 sudo nixos-rebuild switch >nixos-switch.log 2>&1
 cat nixos-switch.log | grep --color error
 if test $status -eq 1
     hyprctl reload
     set gen (readlink /nix/var/nix/profiles/system | grep -o "[0-9]*")
-    git add .
     git commit -am "Generation #$gen"
     git push &>/dev/null
+else
+    git reset &>/dev/null
 end
 popd
