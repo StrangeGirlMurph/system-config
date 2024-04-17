aef680951455865a70d3f94f9b3f44d7d1ba7a30
Murphy <git@murphy-in.space>
2024-04-17 15:35:44 +0200

    Generation #20

--- core/display-manager.nix
+++ core/display-manager.nix
@@ -1,15 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    where-is-my-sddm-theme
+  ];
+
   # SDDM
   services.displayManager.sddm = {
     enable = true;
-    theme = "${(pkgs.fetchFromGitHub {
-      owner = "MarianArlt";
-      repo = "kde-plasma-chili";
-      rev = "a371123959676f608f01421398f7400a2f01ae06";
-      sha256 = "17pkxpk4lfgm14yfwg6rw6zrkdpxilzv90s48s2hsicgl3vmyr3x";
-    })}";
+    theme = "where_is_my_sddm_theme";
     autoNumlock = true;
   };
 }
