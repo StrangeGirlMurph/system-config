4758751c3e770b4a75bec27bf2f6ab3a93c54b34
Murphy <git@murphy-in.space>
2024-04-17 16:34:55 +0200

    Generation #31

--- core/display-manager.nix
+++ core/display-manager.nix
@@ -1,15 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    where-is-my-sddm-theme
-    elegant-sddm
-  ];
-
-  # SDDM
-  services.displayManager.sddm = {
-    enable = true;
-    theme = "where is my sddm theme";
-    autoNumlock = true;
-  };
+  services.displayManager.sddm.enable = true;
 }
