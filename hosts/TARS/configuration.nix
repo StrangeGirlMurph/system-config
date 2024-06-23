9a04c7dc4472695019c079fcb9cd71a4b6d7dfaf
Murphy <git@murphy-in.space>
2024-06-23 15:05:51 +0200

    few changes

--- hosts/TARS/configuration.nix
+++ hosts/TARS/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [
-      ./hardware-configuration.nix
+      ../../hardware-configuration.nix
     ];
 
   networking.hostName = "TARS";
--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -z "$IN_NIX_SHELL" ]; then
-  echo "Entering nix-shell with git"
-  exec nix-shell -p git --command "$0 $@"
-fi
-
+nix-shell -p git --run '
 cd ~
+echo "Cloning system-config..."
 git clone https://github.com/StrangeGirlMurph/system-config
+echo "Moving hardware-configuration.nix..."
 sudo mv -f /etc/nixos/hardware-configuration.nix ~/system-config/
+echo "Symlinking system-config..."
 sudo rm -rf /etc/nixos
 sudo ln -s ~/system-config/ /etc/nixos
-read -p 'Configuration: ' config
+read -p "Pick which configuration you want (TARS or CASE): " config
+echo "Building system..."
 sudo nixos-rebuild switch --flake system-config/#${config}
+'
