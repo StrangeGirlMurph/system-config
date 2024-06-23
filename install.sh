9c8a14f8f328270e27334fdb22bbf79b24cc8ee4
Murphy Sünnenwold <git@murphy-in.space>
2024-06-23 14:40:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-nix-shell -p git --run 'bash -c "
+if [ -z "$IN_NIX_SHELL" ]; then
+  echo "Entering nix-shell with git"
+  exec nix-shell -p git --command "$0 $@"
+fi
+
 cd ~
 git clone https://github.com/StrangeGirlMurph/system-config
 sudo mv /etc/nixos/hardware-configuration.nix ~/system-config/
@@ -8,4 +12,3 @@ sudo rm -rf /etc/nixos
 sudo ln -s ~/system-config/ /etc/nixos
 read -p 'Configuration: ' config
 sudo nixos-rebuild switch --flake system-config/#"$config"
-"'
