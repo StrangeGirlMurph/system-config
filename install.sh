63a9a00b414efaf50235ab3a74b3612cd8e15ebb
Murphy Sünnenwold <git@murphy-in.space>
2024-06-23 14:46:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ fi
 
 cd ~
 git clone https://github.com/StrangeGirlMurph/system-config
-sudo mv /etc/nixos/hardware-configuration.nix ~/system-config/
+sudo mv -f /etc/nixos/hardware-configuration.nix ~/system-config/
 sudo rm -rf /etc/nixos
 sudo ln -s ~/system-config/ /etc/nixos
 read -p 'Configuration: ' config
-sudo nixos-rebuild switch --flake system-config/#"$config"
+sudo nixos-rebuild switch --flake system-config/#${config}
