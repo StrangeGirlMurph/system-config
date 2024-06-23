5cab3f58d2da843cc37249615810bbb6c0e038df
Murphy Sünnenwold <git@murphy-in.space>
2024-06-23 16:15:25 +0200

    hopefully this works

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-nix-shell -p git --run '
-cd ~
-config = "$1"
-echo "Cloning system-config..."
-git clone https://github.com/StrangeGirlMurph/system-config
-echo "Moving hardware-configuration.nix..."
-sudo mv -f /etc/nixos/hardware-configuration.nix ~/system-config/hosts/${config}/
-git add . 
-echo "Symlinking system-config..."
-sudo rm -rf /etc/nixos
-sudo ln -s ~/system-config/ /etc/nixos
-echo "Building system..."
-sudo nixos-rebuild switch --flake system-config/#${config}
-'
+config=$1 
+
+nix-shell -p git --run "
+    cd ~
+    echo 'Cloning system-config...'
+    git clone https://github.com/StrangeGirlMurph/system-config
+    echo 'Moving hardware-configuration.nix...'
+    sudo mv -f /etc/nixos/hardware-configuration.nix ~/system-config/hosts/$config/
+    git -C ~/system-config add -A
+    echo 'Symlinking system-config...'
+    sudo rm -rf /etc/nixos
+    sudo ln -s ~/system-config/ /etc/nixos
+    echo 'Building system...'
+    sudo nixos-rebuild switch --flake system-config/#$config
+"
